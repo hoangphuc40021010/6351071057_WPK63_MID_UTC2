24f0e01b444720ba621da8254a304794352fe43f
hoangphuc40021010 <hoangphuc40021010@gmail.com>
2024-09-27 17:09:05 +0700

    update

---
+++ Detail.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/DefaultPageMaster.Master" AutoEventWireup="true" CodeBehind="Detail.aspx.cs" Inherits="de1.Detail" %>
+<asp:Content ID="detail" ContentPlaceHolderID="detailContent" runat="server">
+    <h3 style="color:red;">Big Data Anakysis  with Spark</h3>
+    <p>-----</p>
+    <br />
+
+    <asp:Repeater ID="rptCourse" runat="server">
+    <ItemTemplate>
+       
+            <h3> <strong>Category Name: <%# Eval("CategoryName") %></strong></h3>
+           
+            <h3<strong style="color: red;"><%# Eval("Duration") %> mins</strong></h3>
+            <p><%# Eval("Description") %> </p>
+            <p>Number of course registrations: <%# Eval("numberView") %></p>
+          
+       
+    </ItemTemplate>
+</asp:Repeater>
+</asp:Content>
---
+++ Detail.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace de1
+{
+    public partial class Detail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Id"] != null)
+            {
+                if (!IsPostBack)
+                {
+                  
+                        QLKhoaHocEntities db = new QLKhoaHocEntities();
+                        try
+                        {
+                            int id = int.Parse(Session["Id"].ToString());
+                        var course = db.Courses
+                         .Where(c => c.ID == id)
+                         .Select(c => new
+                         {
+
+                             CategoryName = c.Category.CatName,
+                             Duration = c.Duration,
+                             Description = c.Description,
+                             numberView = c.NumViews
+                         }).ToList();
+                        rptCourse.DataSource = course;
+                            rptCourse.DataBind();
+                        }
+                        catch (Exception ex)
+                        {
+                            Response.Write(ex.Message);
+                        }
+                    
+
+                }
+            }
+
+        }
+    }
+}
